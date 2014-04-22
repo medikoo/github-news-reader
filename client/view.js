e329d69a9e839d0094eba859fdeefeec51261d88
Mariusz Nowak <mariusz@medikoo.com>
2014-04-22 18:04:15 +0200

    -- lint

--- client/view.js
+++ client/view.js
@@ -1,33 +1,33 @@
 'use strict';
 
-var isArray    = Array.isArray
-  , call       = Function.prototype.call
-  , max        = Math.max
-  , keys       = Object.keys
-  , last       = require('es5-ext/array/#/last')
-  , format     = require('es5-ext/date/#/format')
-  , pluck      = require('es5-ext/function/pluck')
-  , not        = require('es5-ext/function/#/not')
-  , count      = require('es5-ext/object/count')
-  , map        = require('es5-ext/object/map')
-  , mapToArray = require('es5-ext/object/to-array')
-  , memoize    = require('memoizee/lib/regular')
-  , lcSort     = call.bind(require('es5-ext/string/#/case-insensitive-compare'))
-  , domjs      = require('domjs/lib/html5')(document)
-  , data       = require('./data')
+var isArray = Array.isArray
+  , call    = Function.prototype.call
+  , max     = Math.max
+  , keys    = Object.keys
+  , last    = require('es5-ext/array/#/last')
+  , format  = require('es5-ext/date/#/format')
+  , pluck   = require('es5-ext/function/pluck')
+  , not     = require('es5-ext/function/#/not')
+  , count   = require('es5-ext/object/count')
+  , toArray = require('es5-ext/object/to-array')
+  , memoize = require('memoizee/lib/regular')
+  , lcSort  = call.bind(require('es5-ext/string/#/case-insensitive-compare'))
+  , domjs   = require('domjs/lib/html5')(document)
+  , data    = require('./data')
 
   , articleDOM;
 
 articleDOM = memoize(function (article) {
-	var el, body
-	el = this.li({ 'class': 'article' },
-		 !article.skipTitle &&
-		 this.h2(this.a({ href: article.link, target: '_blank' }, article.title)),
-		 !article.skipAuthor && this.div({ 'class': 'author' }, this.b(article.author), ' at ' +
-			 format.call(new Date(Date.parse(article.date)), '%Y-%m-%d %H:%M:%S')),
-		 body = this.div({ 'class': 'body' })())();
-	body.innerHTML = article.description ?
-			article.description.replace(/<a href=/g, '<a target="_blank" href=') : '';
+	var el, body;
+	el = this.li({ class: 'article' },
+		!article.skipTitle &&
+		this.h2(this.a({ href: article.link, target: '_blank' }, article.title)),
+		!article.skipAuthor && this.div({ class: 'author' },
+			this.b(article.author), ' at ' +
+			format.call(new Date(Date.parse(article.date)), '%Y-%m-%d %H:%M:%S')),
+		body = this.div({ class: 'body' })())();
+	body.innerHTML = article.description
+		? article.description.replace(/<a href=/g, '<a target="_blank" href=') : '';
 	return el;
 }.bind(domjs.map));
 
@@ -69,7 +69,7 @@ document.body.appendChild(domjs.build(function () {
 	};
 
 	ignore = function () {
-		current && current.emit('ignore');
+		if (current) current.emit('ignore');
 	};
 
 	fixPadding = function () {
@@ -81,42 +81,44 @@ document.body.appendChild(domjs.build(function () {
 	};
 
 	section({ 'class': 'aside' },
-		ul({ 'class': 'nest-' + nest }, mapToArray(data, function self(value, key, context) {
-			var el, len;
-			if (isArray(value)) {
-				el = li({ 'class': 'feed' }, a({ onclick: load.bind(value) },
-					 (last.call(value).headTitle || key) + "\u00a0(",
-					 (len = _text(value.filter(not.call(pluck('read'))).length)), ")"))();
-				value.on('select', function () {
-					if (selected) {
-						selected.classList.remove('selected');
-					}
-					el.classList.add('selected');
-					selected = el;
-				});
-				value.on('update', function () {
-					var rlen = this.filter(not.call(pluck('read'))).length;
-					len.data = rlen;
-					if (!rlen) {
-						el.parentNode.removeChild(el);
-					}
-				});
-				value.on('ignore', function () {
-					if (el.parentNode) el.parentNode.removeChild(el);
-					reset();
-				});
-			} else {
-				el = li(h3(key), ul({ 'class': 'nest-' + (++nest) },
-					 mapToArray(value, self, null, lcSort)))();
-				--nest;
-				value.on('update', function () {
-					if (!count(this)) {
-						el.parentNode.removeChild(el);
-					}
-				});
-			}
-			return el;
-		}, null, lcSort)));
+		ul({ 'class': 'nest-' + nest },
+			toArray(data, function self(value, key, context) {
+				var el, len;
+				if (isArray(value)) {
+					el = li({ 'class': 'feed' }, a({ onclick: load.bind(value) },
+						(last.call(value).headTitle || key) + "\u00a0(",
+						(len = _text(value.filter(not.call(pluck('read'))).length)),
+						")"))();
+					value.on('select', function () {
+						if (selected) {
+							selected.classList.remove('selected');
+						}
+						el.classList.add('selected');
+						selected = el;
+					});
+					value.on('update', function () {
+						var rlen = this.filter(not.call(pluck('read'))).length;
+						len.data = rlen;
+						if (!rlen) {
+							el.parentNode.removeChild(el);
+						}
+					});
+					value.on('ignore', function () {
+						if (el.parentNode) el.parentNode.removeChild(el);
+						reset();
+					});
+				} else {
+					el = li(h3(key), ul({ 'class': 'nest-' + (++nest) },
+						toArray(value, self, null, lcSort)))();
+					--nest;
+					value.on('update', function () {
+						if (!count(this)) {
+							el.parentNode.removeChild(el);
+						}
+					});
+				}
+				return el;
+			}, null, lcSort)));
 	container = section({ 'class': 'content' },
 		content = div(
 			p({ 'class': 'controls' },
@@ -130,7 +132,7 @@ document.body.appendChild(domjs.build(function () {
 	window.onresize = fixPadding;
 
 	container.onscroll = function () {
-		var index = -1, pos = container.scrollTop;
+		var pos = container.scrollTop;
 		offsets.every(function (offset, i) {
 			if (pos > offset) {
 				current[i].markRead();
