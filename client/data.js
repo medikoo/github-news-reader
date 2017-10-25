42bc58c0f2550591f622544f008776f4f7d8f398
Mariusz Nowak <medyk@medikoo.com>
2017-10-25 11:39:11 +0200

    style: address new lint rules

--- client/data.js
+++ client/data.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-var isArray = Array.isArray
-  , pluck   = require('es5-ext/function/pluck')
-  , not     = require('es5-ext/function/#/not')
-  , count   = require('es5-ext/object/count')
-  , forEach = require('es5-ext/object/for-each')
-  , map     = require('es5-ext/object/map')
-  , ee      = require('event-emitter')
+let isArray = Array.isArray
+  , pluck   = require("es5-ext/function/pluck")
+  , not     = require("es5-ext/function/#/not")
+  , count   = require("es5-ext/object/count")
+  , forEach = require("es5-ext/object/for-each")
+  , map     = require("es5-ext/object/map")
+  , ee      = require("event-emitter")
 
   , markRead, data;
 
 markRead = function () {
 	if (!this.read) {
 		this.read = true;
-		this.emit('update', { type: 'read' });
+		this.emit("update", { type: "read" });
 	}
 };
 
@@ -24,38 +24,40 @@ console.log("DATA", data);
 module.exports = exports = map(data, function self(data, name) {
 	if (isArray(data)) {
 		data = ee(data);
-		data.forEach(function (article) {
+		data.forEach(article => {
 			ee(article);
-			article.on('update', function () {
-				data.emit('update');
+			article.on("update", () => {
+				data.emit("update");
 			});
 			article.markRead = markRead;
 		});
-		data.on('ignore', function () { delete exports[name]; });
-		data.on('update', function () {
-			if (!this.filter(not.call(pluck('read'))).length) {
+		data.on("ignore", () => {
+ delete exports[name];
+});
+		data.on("update", function () {
+			if (!this.filter(not.call(pluck("read"))).length) {
 				delete exports[name];
 			}
 		});
 	} else {
 		data = ee(map(data, self));
-		forEach(data, function (obj, name) {
+		forEach(data, (obj, name) => {
 			if (isArray(obj)) {
-				obj.on('update', function () {
-					if (!this.filter(not.call(pluck('read'))).length) {
+				obj.on("update", function () {
+					if (!this.filter(not.call(pluck("read"))).length) {
 						delete data[name];
-						data.emit('update');
+						data.emit("update");
 					}
 				});
-				obj.on('ignore', function () {
+				obj.on("ignore", () => {
 					delete data[name];
-					data.emit('update');
+					data.emit("update");
 				});
 			} else {
-				obj.on('update', function () {
+				obj.on("update", function () {
 					if (!count(this)) {
 						delete data[name];
-						data.emit('update');
+						data.emit("update");
 					}
 				});
 			}
@@ -64,8 +66,8 @@ module.exports = exports = map(data, function self(data, name) {
 	return data;
 });
 
-forEach(exports, function (obj, name) {
-	obj.on('update', function () {
+forEach(exports, (obj, name) => {
+	obj.on("update", function () {
 		if (!count(this)) {
 			delete exports[name];
 		}
